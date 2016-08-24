97bfa893696f2927d7423c4165a8d4cf852c302e
Lohit Audhkhasi <audhkhasi.lohit@gmail.com>
2016-08-24 10:34:17 +0530

    Initial commit

---
+++ src/main/java/com/spring/web/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.spring.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String welcomeHandler(ModelMap map) {
+		map.put("entered", "true");
+		return "home";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/jsp/home.jsp
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Spring Practice Tool</title>
+	</head>
+	<body>
+		<div>
+			<a href="empMgmt">Employee Management</a>
+		</div>
+		<div>
+			<a href="projMgmt">Project Management</a>
+		</div>
+	</body>
+</html>
